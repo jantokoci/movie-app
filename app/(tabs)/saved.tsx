007efde1b1969cf012e43fa2148908d01c95a4d2
jantokoci <153398095+jantokoci@users.noreply.github.com>
2025-05-22 00:27:25 +0200

    movieapp

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Saved = () => {
+    return(
+        <View>
+            <Text>Saved</Text>
+        </View>
+    )
+}
+export default Saved;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MovieDetails = () => {
+    return(
+        <View>
+            <Text>MovieDetails</Text>
+        </View>
+    )
+}
+export default MovieDetails;
